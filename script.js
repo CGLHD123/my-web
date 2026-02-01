beec466a2c185f9eccd7c64768f39fe9da5e1a9d
LHD001 <lhdclone123@gmail.com>
2026-02-01 18:10:25 +0700

    hoan thien code co ban

---
+++ script.js
@@ -0,0 +1,7 @@
+// Lấy thẻ button theo ID
+const btn = document.getElementById('btn-click');
+
+// Lắng nghe sự kiện nhấn chuột
+btn.addEventListener('click', function () {
+    alert('Cảm ơn bạn đã ghé thăm trang web của mình!');
+});
